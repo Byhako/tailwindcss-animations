43a3a2992ede3dc5ea39af7c09c0a993bcc8e045
Ruben E Acosta <reacosta@grupo-exito.com>
2024-02-15 07:17:37 -0500

    delay and duration

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import theme from './theme.js'
 
 /** @type {import('tailwindcss/types/config').PluginCreator} */
 const pluginCreator = (api) => {
-  console.info('creator')
+  const { theme, matchUtilities } = api
+
+  matchUtilities({
+    'animate-delay': value => ({
+      'animation-delay': value
+    })
+  }, {
+    values: theme('animationDelay')
+  })
+
+  matchUtilities({
+    'animate-duration': value => ({
+      'animation-duration': value
+    })
+  }, {
+    values: theme('animationDuration')
+  })
 }
 
 /** @type {import('tailwindcss/types/config').Config} */
--- test/index.test.js
+++ test/index.test.js
@@ -9,4 +9,36 @@ describe('tailwind animations plugin', () => {
 
     expect(css).toMatch('@keyframes zoom-in{0%{opacity:0;transform:scale(.5)}100%{opacity:1;transform:scale(1)}}.animate-zoom-in{animation:zoom-in 0.6s ease-out}')
   })
+
+  it('use a predefined animation delay', async () => {
+    const css = await generatePluginCSS({
+      content: '<div class="animate-delay-300">Ruben</div>'
+    })
+
+    expect(css).toMatch('.animate-delay-300{animation-delay:300ms}')
+  })
+
+  it('use a custom animation delay', async () => {
+    const css = await generatePluginCSS({
+      content: '<div class="animate-delay-[341ms]">Ruben</div>'
+    })
+
+    expect(css).toMatch('.animate-delay-\\[341ms\\]{animation-delay:341ms}')
+  })
+
+  it('use a predefined animation duration', async () => {
+    const css = await generatePluginCSS({
+      content: '<div class="animate-duration-300">Ruben</div>'
+    })
+
+    expect(css).toMatch('.animate-duration-300{animation-duration:300ms}')
+  })
+
+  it('use a custom animation duration', async () => {
+    const css = await generatePluginCSS({
+      content: '<div class="animate-duration-[341ms]">Ruben</div>'
+    })
+
+    expect(css).toMatch('.animate-duration-\\[341ms\\]{animation-duration:341ms}')
+  })
 })
